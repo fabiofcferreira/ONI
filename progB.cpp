d951031674c4a470d1b141bdaa2a7844ba42d99b
Fábio Ferreira <ffcfpten@gmail.com>
2017-03-31 17:38:05 +0100

    Update progB.cpp

--- progB.cpp
+++ progB.cpp
@@ -4,38 +4,31 @@
 using namespace std;
 
 int main() {
-  int n = 0,
-      q = 0,
-      i = 0;
-  long x0, x1, x2, y1, y2;
+    int n = 0,
+        q = 0,
+        a = 1,
+        b = 1;
+    long x1, x2, y1, y2;
+    int pos[q+1] = {0};
 
-  scanf("%d %d", &n, &q);
-  vector< vector<long> > charcos(n, vector<long>(0));
-
-  for(i = 0; i <= n-1; i++) {
-    scanf("%li %li %li %li", &x1, &x2, &y1, &y2);
-    charcos[i].push_back(x1);
-    charcos[i].push_back(x2);
-    charcos[i].push_back(y1);
-    charcos[i].push_back(y2);
-  }
-  sort(charcos.begin(), charcos.end());
-
-  for(i = q; i > 0; i--) {
-    scanf("%li", &x0);
-
-    int a = 0, b = n;
-    while(a <= b) {
-      int k = (a+b)/2;
-      if(charcos[k][0] == x0) {
-        // found rect
-        break;
+    scanf("%d %d", &n, &q);
+    //vector< vector<long> >charcos(n+1);
+    vector<int> Qpedidos(q+1);
+    
+    for(; a <= n; a++) {
+      scanf("%li %li %li %li", &x1, &y1, &x2, &y2);
+      /*
+      charcos[i].push_back(x1);
+      charcos[i].push_back(x2 - x1);
+      charcos[i].push_back(y2 - y1);
+      */
+      for(b = x1; b < x2; b++) {
+          printf("Q = %d\n", b);
+          printf("Before: %d\n", pos[b]);
+          printf("Altura a adicionar: %li\n", (y2 - y1));
+          pos[b] += (y2 - y1);
+          printf("After: %d\n", pos[b]);
       }
-      if(charcos[k][0] > x0) b = k-1;
-      else a = k + 1;
     }
-
-  }
-
-  return 0;
+    return 0;
 }
