4fec97e1cb7f7e12b8b44cdf54e317ebffbb0dae
Fábio Ferreira <ffcfpten@gmail.com>
2017-08-04 03:09:44 +0100

    B.cpp

---
+++ 2017/qual/B.cpp
@@ -0,0 +1,36 @@
+#include <cstdio>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    int n = 0,
+        q = 0;
+    int a = 1, b = 1;
+    long x1, x2, y1, y2, xi;
+    scanf("%d %d", &n, &q);
+    int pos[100001] = {};
+
+    printf("%d\n", pos[1]);
+    printf("%d\n", pos[2]);
+
+    queue<int> Qpedidos;
+
+    for(; a <= n; a++) {
+      scanf("%li %li %li %li", &x1, &y1, &x2, &y2);
+      for(b = x1; b < x2; b++) {
+          pos[b] += (y2 - y1);
+      }
+    }
+    for(b = q; b; b--) {
+      scanf("%li", &xi);
+      Qpedidos.push(xi);
+    }
+    for(b = q; b; b--) {
+      printf("%d\n", pos[Qpedidos.front()]);
+      Qpedidos.pop();
+    }
+
+    return 0;
+}
