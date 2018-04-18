894ef2b95faa6b1707f4b2cc907f33f056b1ab20
Fábio Ferreira <ffcfpten@gmail.com>
2018-04-18 23:28:40 +0100

    10550 - not solved but started

---
+++ UVa/CP3/Intro/The Easy Problems/Super Easy/10550.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+  int initPos = 0, x = 0, y = 0, z = 0, degrees = 0;
+
+  cin >> initPos >> x >> y >> z;
+  //cout << "Init pos: " << initPos << " X: " << x << " Y: " << y << " Z: " << z;
+
+  // turn the dial clockwise 2 full turns
+  degrees += 720;
+
+  
+  return 0;
+}
