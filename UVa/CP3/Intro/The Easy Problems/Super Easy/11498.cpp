b8f8aaa0c6629e547e25bfad3c4762b28fa3d816
Fábio Ferreira <ffcfpten@gmail.com>
2018-05-15 19:47:43 +0100

    solved: 11498 division of nlogonia

---
+++ UVa/CP3/Intro/The Easy Problems/Super Easy/11498.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    while(true) {
+        int t;
+        cin >> t;
+        if(!t) break;
+
+        int divX, divY;
+        cin >> divX >> divY;
+
+        
+        for(; t; t--) {
+            int x, y;
+            bool divisa = false;
+            string quadrant = "";
+            cin >> x >> y;
+
+            // Y coord is greater than division Y coord, thus the house in the north quadrants
+            if(y > divY) quadrant = "N";
+            else if (y < divY) quadrant = "S";
+            else divisa = true;
+
+            // the same happens with coords X
+            if(x > divX) quadrant += "E";
+            else if (x < divX) quadrant += "O";
+            else divisa = true;
+
+            divisa ? cout << "divisa\n" : cout << quadrant << "\n";
+        }
+        
+    }
+
+    return 0;
+}
