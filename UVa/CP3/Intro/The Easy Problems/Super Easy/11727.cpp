0fdf45d07663f3477498f8e8b6478520f512275a
Fábio Ferreira <ffcfpten@gmail.com>
2018-05-15 20:25:08 +0100

    solved: 11727 - cost cutting

---
+++ UVa/CP3/Intro/The Easy Problems/Super Easy/11727.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    for(int i = 1; i <= t; i++) {
+        //vector<int> saleries;
+        int s1, s2, s3, saved;
+        cin >> s1 >> s2 >> s3;
+
+        if(s1 > s2) {
+            if(s1 > s3) s2 > s3 ? saved = s2 : saved = s3;
+            else saved = s1;
+        } else if(s2 > s1) {
+            if(s2 > s3) s3 > s1 ? saved = s3 : saved = s1;
+            else saved = s2;
+        }
+
+        cout << "Case " << i << ": " << saved << "\n"; 
+    }
+
+    return 0;
+}
