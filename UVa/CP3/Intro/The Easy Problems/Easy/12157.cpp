95fc2b1c0d5686140839d6a9d93222240b73d7a8
Fábio Ferreira <ffcfpten@gmail.com>
2018-05-22 00:25:38 +0100

    solved: 12157 - tariff plan

---
+++ UVa/CP3/Intro/The Easy Problems/Easy/12157.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    for(int i = 1; i <= t; i++) {
+        int n;
+        cin >> n;
+        int calls[n];
+
+        for(int k = 0; k < n; k++) {
+            int callDuration;
+            cin >> callDuration;
+
+            calls[k] = callDuration;
+        }
+
+        int mileCost = 0, juiceCost = 0;
+        for(int k = 0; k < n; k++) {
+            mileCost += ((calls[k] / 30) * 10) + 10; 
+            juiceCost += ((calls[k] / 60) * 15) + 15;
+        }
+
+
+        cout << "Case " << i << ": ";
+        if(mileCost < juiceCost) cout << "Mile ";
+        else if(mileCost > juiceCost) cout << "Juice ";
+        else cout << "Mile Juice ";
+
+        cout << min(mileCost, juiceCost) << "\n";
+    }
+
+    return 0;
+}
